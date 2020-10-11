42574d38cf4a570158eb83288d703fa094f9a4bc
Drew hoffer <drew_hoffer@hotmail.com>
2020-10-11 11:12:56 -0400

    Start styling

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { Container } from "semantic-ui-react";
+
+
 import Header from "./Header";
-import Footer from "./Footer";
 
 export const siteTitle = "Task Manager App";
 
@@ -12,11 +14,18 @@ export default function Layout ({ children, user }) {
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="og:title" content={siteTitle} />
+				<link rel="stylesheet" type="text/css" href="/static/styles.css" />
+				<link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+				<link
+					rel="stylesheet"
+					href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
+				/>
 				<title>{siteTitle}</title>
 			</Head>
 			<Header user={user}/>
-			{children}
-			<Footer />
+			<Container text style= {{ paddingTop: "1em"}} >
+				{children}
+			</Container>
 		</>
 	);
 }
